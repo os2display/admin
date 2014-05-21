ceea7ec4e3070014a12701501317edf8d518ab7d
Troels Ugilt Jensen <tujensen@gmail.com>
2014-05-21 14:36:01 +0200

    Added channels

--- js/channel.js
+++ js/channel.js
@@ -7,7 +7,19 @@ function ChannelCtrl($scope) {
     orientation: '',
     slides: ''
   };
+  $scope.changeOrientationHorizontal = function() {
+    $scope.channel.orientation = 'horizontal';
+    $scope.editor.orientationHorizontalSelected = 'selected';
+    $scope.editor.orientationVerticalSelected = '';
+  };
+  $scope.changeOrientationVertical = function() {
+    $scope.channel.orientation = 'vertical';
+    $scope.editor.orientationHorizontalSelected = '';
+    $scope.editor.orientationVerticalSelected = 'selected';
+  };
   $scope.editor = {
+    orientationHorizontalSelected: '',
+    orientationVerticalSelected: '',
     page: 1
-  }
+  };
 }
