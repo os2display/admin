be7de66e2d664402ffe2456c6cc38e0bec0af7fa
martinyde <yde001@gmail.com>
2014-05-21 14:36:47 +0200

    Added mockups and sass

---
+++ mockups/dashboard.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <?php include 'inc/head.inc'; ?>
+  <header class="header" role="banner">
+    <div class="header--inner">
+      <?php include 'inc/header-main.inc'; ?>
+    </div>
+  </header>
+</html>
