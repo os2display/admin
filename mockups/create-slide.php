26869cd800aa53f4103ef218c3ca7f52e8327b34
martinyde <yde001@gmail.com>
2014-05-23 14:00:22 +0200

    Added styling for animations

--- mockups/create-slide.php
+++ mockups/create-slide.php
@@ -98,8 +98,8 @@
           <div class="create-slide--config-text-wrapper">
             <div class="create-slide--config-text">
               <a class="create-slide--edit-box-settings" href="#"></a>
-              <h1 class="slide--header">My awesome slide<a class="create-slide--edit-title" href="#"></a></h1>
-              <div class="slide--description">
+              <h1 class="create-slide--config-header">My awesome slide<a class="create-slide--edit-title" href="#"></a></h1>
+              <div class="create-slide--config-description">
                 This is the content of the slide, edit this text. The bounding box will expand accoding to how much text you add. Note that the maximum height of this textbox is 50% of the total height of the slide.
                 <a class="create-slide--edit-description" href="#"></a>
               </div>
