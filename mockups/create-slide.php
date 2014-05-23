8f6b3b985b2b2d854fea31fecba5bbd5fa62ee20
martinyde <yde001@gmail.com>
2014-05-23 11:34:27 +0200

    Added stling for create slide four

--- mockups/create-slide.php
+++ mockups/create-slide.php
@@ -94,17 +94,19 @@
       <div class="create-slide--content--slide-four">
         <h1 class="create-slide--header">Opret slide (4/4)</h1>
         <p class="create-slide--header-description">Tilføj indhold - husk at se dit indhold på alle dine skærme.</p>
-        <div class="create-slide--config">
+        <div class="create-slide--config" style="height:540px;width: 940px;font-size: 18px;">
           <div class="create-slide--config-text-wrapper">
-            <a class="create-slide--edit-box-settings" href="#"></a>
-            <h1 class="slide--header">My awesome slide<a class="create-slide--edit-title" href="#"></a></h1>
-            <div class="slide--description">
-              This is the content of the slide, edit this text. The bounding box will expand accoding to how much text you add. Note that the maximum height of this textbox is 50% of the total height of the slide.
-              <a class="create-slide--edit-description" href="#"></a>
+            <div class="create-slide--config-text">
+              <a class="create-slide--edit-box-settings" href="#"></a>
+              <h1 class="slide--header">My awesome slide<a class="create-slide--edit-title" href="#"></a></h1>
+              <div class="slide--description">
+                This is the content of the slide, edit this text. The bounding box will expand accoding to how much text you add. Note that the maximum height of this textbox is 50% of the total height of the slide.
+                <a class="create-slide--edit-description" href="#"></a>
+              </div>
             </div>
           </div>
           <a class="create-slide--edit-image-settings" href="#"></a>
-          <img src="/images/outlines/slide-config-default.png"/>
+          <img src="/images/outlines/slide-config-default.png" class="create-slide--image"/>
         </div>
         <input type="submit" class="create-slide--button" value="Gem og opret">
       </div>
