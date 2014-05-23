4587102ee203e408920ed28db9eeb953e3dc9458
martinyde <yde001@gmail.com>
2014-05-23 09:12:27 +0200

    Added mockups for create slide

--- mockups/create-slide.php
+++ mockups/create-slide.php
@@ -23,13 +23,17 @@
         <div class="create-slide--screens">
           <div class="create-slide--screen-landscape">
             <a href="#" class="create-slide--screen-link is-selected">
-              <span class="create-slide--screen-link-check"></span>
+              <span class="create-slide--overlay">
+                <span class="create-slide--link-check"></span>
+              </span>
               <img src="/images/outlines/landscape-outline.png"/>
             </a>
           </div>
           <div class="create-slide--screen-portrait">
             <a href="#" class="create-slide--screen-link">
-              <span class="create-slide--screen-link-check"></span>
+              <span class="create-slide--overlay">
+                <span class="create-slide--link-check"></span>
+              </span>
               <img src="/images/outlines/portrait-outline.png" />
             </a>
           </div>
@@ -44,31 +48,41 @@
         <div class="create-slide--templates">
           <div class="create-slide--template">
             <a href="#" class="create-slide--template-link">
-              <span class="create-slide--template-link-check"></span>
+              <span class="create-slide--overlay">
+                <span class="create-slide--link-check"></span>
+              </span>
               <img src="/images/outlines/template-example-1.png"/>
             </a>
           </div>
           <div class="create-slide--template">
             <a href="#" class="create-slide--template-link is-selected">
-              <span class="create-slide--template-link-check"></span>
+              <span class="create-slide--overlay">
+                <span class="create-slide--link-check"></span>
+              </span>
               <img src="/images/outlines/template-example-2.png"/>
             </a>
           </div>
           <div class="create-slide--template">
             <a href="#" class="create-slide--template-link">
-              <span class="create-slide--template-link-check"></span>
+              <span class="create-slide--overlay">
+                <span class="create-slide--link-check"></span>
+              </span>
               <img src="/images/outlines/template-example-3.png"/>
             </a>
           </div>
           <div class="create-slide--template">
             <a href="#" class="create-slide--template-link">
-              <span class="create-slide--template-link-check"></span>
+              <span class="create-slide--overlay">
+                <span class="create-slide--link-check"></span>
+              </span>
               <img src="/images/outlines/template-example-4.png"/>
             </a>
           </div>
           <div class="create-slide--template">
             <a href="#" class="create-slide--template-link">
-              <span class="create-slide--template-link-check"></span>
+              <span class="create-slide--overlay">
+                <span class="create-slide--link-check"></span>
+              </span>
               <img src="/images/outlines/template-example-5.png"/>
             </a>
           </div>
@@ -80,6 +94,19 @@
       <div class="create-slide--content--slide-four">
         <h1 class="create-slide--header">Opret slide (4/4)</h1>
         <p class="create-slide--header-description">Tilføj indhold - husk at se dit indhold på alle dine skærme.</p>
+        <div class="create-slide--config">
+          <div class="create-slide--config-text-wrapper">
+            <a class="create-slide--edit-box-settings" href="#"></a>
+            <h1 class="slide--header">My awesome slide<a class="create-slide--edit-title" href="#"></a></h1>
+            <div class="slide--description">
+              This is the content of the slide, edit this text. The bounding box will expand accoding to how much text you add. Note that the maximum height of this textbox is 50% of the total height of the slide.
+              <a class="create-slide--edit-description" href="#"></a>
+            </div>
+          </div>
+          <a class="create-slide--edit-image-settings" href="#"></a>
+          <img src="/images/outlines/slide-config-default.png"/>
+        </div>
+        <input type="submit" class="create-slide--button" value="Gem og opret">
       </div>
     </section>
   </div>
