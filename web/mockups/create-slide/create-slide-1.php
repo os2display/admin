f0e00479e3fe91f450d22905d4dee7f7c21d5d6d
martinyde <yde001@gmail.com>
2014-06-12 14:03:17 +0200

    Added nav menu

--- web/mockups/create-slide/create-slide-1.php
+++ web/mockups/create-slide/create-slide-1.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html>
   <?php include '../inc/head.inc'; ?>
-  <header class="header" role="banner">
-    <div class="header--inner">
-      <?php include '../inc/header-main.inc'; ?>
-    </div>
-    <?php include '../inc/header-sub.inc'; ?>
-  </header>
-  <div class="layout-wrapper">
-    <section id="create-slide-one" class="create-slide--section">
-      <div class="create-slide--content--slide-one">
-        <h1 class="create-slide--header">Opret slide (1/4)</h1>
-        <p class="create-slide--header-description">Giv dit slide et godt navn så du kan finde det igen senere.</p>
-        <input type="text" class="create-slide--text is-valid" placeholder="Skriv f.eks. Frokost uge 10, Billeder fra DHL eller lignende">
-        <input type="submit" class="create-slide--button is-inactive" value="Gem og fortsæt">
+  <body>
+    <a class="nav-overlay"></a>
+    <?php include '../inc/nav-menu.inc'; ?>
+    <header class="header" role="banner">
+      <div class="header--inner">
+        <?php include '../inc/header-main.inc'; ?>
       </div>
-    </section>
-  </div>
+      <?php include '../inc/header-sub.inc'; ?>
+    </header>
+    <div class="layout-wrapper">
+      <section id="create-slide-one" class="create-slide--section">
+        <div class="create-slide--content--slide-one">
+          <h1 class="create-slide--header">Opret slide (1/4)</h1>
+          <p class="create-slide--header-description">Giv dit slide et godt navn så du kan finde det igen senere.</p>
+          <input type="text" class="create-slide--text is-valid" placeholder="Skriv f.eks. Frokost uge 10, Billeder fra DHL eller lignende">
+          <input type="submit" class="create-slide--button is-inactive" value="Gem og fortsæt">
+        </div>
+      </section>
+    </div>
+  </body>
 </html>
--- web/mockups/create-slide/create-slide-4.php
+++ web/mockups/create-slide/create-slide-4.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
 <html>
   <?php include '../inc/head.inc'; ?>
-  <header class="header" role="banner">
-    <div class="header--inner">
-      <?php include '../inc/header-main.inc'; ?>
-    </div>
-    <?php include '../inc/header-sub.inc'; ?>
-  </header>
-  <div class="layout-wrapper">
-    <section id="create-slide-four" class="create-slide--section">
-      <div class="create-slide--content--slide-four">
-        <h1 class="create-slide--header">Opret slide (4/4)</h1>
-        <p class="create-slide--header-description">Tilføj indhold - husk at se dit indhold på alle dine skærme.</p>
-        <div class="create-slide--config" style="height:540px;width: 940px;font-size: 18px;">
-          <div class="create-slide--config-text-wrapper">
-            <div class="create-slide--config-text">
-              <a class="create-slide--edit-box-settings" href="#"></a>
-              <h1 class="create-slide--config-header">My awesome slide<a class="create-slide--edit-title" href="#"></a></h1>
-              <div class="create-slide--config-description">
-                This is the content of the slide, edit this text. The bounding box will expand accoding to how much text you add. Note that the maximum height of this textbox is 50% of the total height of the slide.
-                <a class="create-slide--edit-description" href="#"></a>
+  <body>
+    <a class="nav-overlay"></a>
+    <?php include '../inc/nav-menu.inc'; ?>
+    <header class="header" role="banner">
+      <div class="header--inner">
+        <?php include '../inc/header-main.inc'; ?>
+      </div>
+      <?php include '../inc/header-sub.inc'; ?>
+    </header>
+    <div class="layout-wrapper">
+      <section id="create-slide-four" class="create-slide--section">
+        <div class="create-slide--content--slide-four">
+          <h1 class="create-slide--header">Opret slide (4/4)</h1>
+          <p class="create-slide--header-description">Tilføj indhold - husk at se dit indhold på alle dine skærme.</p>
+          <div class="create-slide--config" style="height:540px;width: 940px;font-size: 18px;">
+            <div class="create-slide--config-text-wrapper">
+              <div class="create-slide--config-text">
+                <a class="create-slide--edit-box-settings" href="#"></a>
+                <h1 class="create-slide--config-header">My awesome slide<a class="create-slide--edit-title" href="#"></a></h1>
+                <div class="create-slide--config-description">
+                  This is the content of the slide, edit this text. The bounding box will expand accoding to how much text you add. Note that the maximum height of this textbox is 50% of the total height of the slide.
+                  <a class="create-slide--edit-description" href="#"></a>
+                </div>
               </div>
             </div>
+            <a class="create-slide--edit-image-settings" href="#"></a>
+            <img src="/images/outlines/slide-config-default.png" class="create-slide--image"/>
           </div>
-          <a class="create-slide--edit-image-settings" href="#"></a>
-          <img src="/images/outlines/slide-config-default.png" class="create-slide--image"/>
+          <input type="submit" class="create-slide--button" value="Gem og opret">
         </div>
-        <input type="submit" class="create-slide--button" value="Gem og opret">
-      </div>
-    </section>
-  </div>
+      </section>
+    </div>
+  </body>
 </html>
