7cc31ef12a1735c5265712295cc1f53ab8ac4c63
Thomas Johansen <thsj@aarhus.dk>
2014-06-19 13:53:44 +0200

    Started working on top menu

--- web/js/controllers/menu.js
+++ web/js/controllers/menu.js
@@ -1,2 +1,8 @@
 ikApp.controller('MenuController', function($scope) {
+  $scope.menu = [
+    {name: 'channels', link: 'channels', title: 'Channels'},
+    {name: 'slides', link: 'slides', title: 'Slides'},
+    {name: 'screens', link: 'screens', title: 'Screens'},
+    {name: 'templates', link: 'templates', title: 'Templates'}
+  ];
 });
