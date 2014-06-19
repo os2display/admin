6f48c9aa470248e02958ef340a0f624dfdfbbf94
Thomas Johansen <thsj@aarhus.dk>
2014-06-19 15:35:54 +0200

    Menu factory added with current path

--- web/js/controllers/menu.js
+++ web/js/controllers/menu.js
@@ -1,4 +1,6 @@
 ikApp.controller('MenuController', function($scope, $location, menuFactory) {
   menuFactory.setLocation($location);
   $scope.menu = menuFactory.getMenuItems();
+
+  console.log($scope.menu);
 });
