b2f2941c46b009c95b734af9c8a6f5541112de11
Thomas Johansen <thsj@aarhus.dk>
2014-06-19 14:59:38 +0200

    Added function to set current path

--- web/js/controllers/menu.js
+++ web/js/controllers/menu.js
@@ -1,3 +1,4 @@
-ikApp.controller('MenuController', function($scope, menuFactory) {
+ikApp.controller('MenuController', function($scope, $location, menuFactory) {
+  menuFactory.setLocation($location);
   $scope.menu = menuFactory.getMenuItems();
 });
