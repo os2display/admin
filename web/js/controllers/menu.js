7871b139fe60988f00d1afab6909083e1a23727d
Troels Ugilt Jensen <tujensen@gmail.com>
2014-06-19 14:43:03 +0200

    Added menu service

--- web/js/controllers/menu.js
+++ web/js/controllers/menu.js
@@ -1,8 +1,3 @@
-ikApp.controller('MenuController', function($scope) {
-  $scope.menu = [
-    {name: 'channels', link: 'channels', title: 'Channels'},
-    {name: 'slides', link: 'slides', title: 'Slides'},
-    {name: 'screens', link: 'screens', title: 'Screens'},
-    {name: 'templates', link: 'templates', title: 'Templates'}
-  ];
+ikApp.controller('MenuController', function($scope, menuFactory) {
+  $scope.menu = menuFactory.getMenuItems();
 });
