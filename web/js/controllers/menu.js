359c3f11d52a3bd4c5579d6b47cc0cd83dd48128
martinyde <yde001@gmail.com>
2014-06-20 10:56:40 +0200

    Added nav menu

--- web/js/controllers/menu.js
+++ web/js/controllers/menu.js
@@ -1,6 +1,15 @@
 ikApp.controller('MenuController', function($scope, $location, menuFactory) {
   menuFactory.setLocation($location);
   $scope.menu = menuFactory.getMenuItems();
+  $scope.menuOpen = null;
 
-  console.log($scope.menu);
+
+  // Navigation menu open / close
+  $scope.toggleMenu = function(){
+    if ($scope.menuOpen === null) {
+      $scope.menuOpen = false;
+    }
+    $scope.menuOpen = !$scope.menuOpen;
+    console.log('123');
+  };
 });
