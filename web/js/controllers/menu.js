4d0cbd174d361f8822d3fe80e5719c04d7cf2798
Troels Ugilt Jensen <tujensen@gmail.com>
2014-06-23 15:42:29 +0200

    Added submenu

--- web/js/controllers/menu.js
+++ web/js/controllers/menu.js
@@ -1,6 +1,70 @@
 ikApp.controller('MenuController', function($scope, $rootScope, $location) {
-  $scope.selectedMenuItem = '';
+  $scope.url = $location.url();
   $scope.navMenuOpen = null;
+  $scope.subMenuItems = [];
+
+  var updateSubMenu = function() {
+    if ($scope.url.indexOf('/channel') == 0) {
+      $scope.subMenuItems = [
+        {
+          title: 'Oversigt',
+          path: '/#/channels',
+          classSuffix: 'overview'
+        },
+        {
+          title: 'Opret kanal',
+          path: '/#/channel',
+          classSuffix: 'create-channel'
+        }
+      ];
+    }
+    else if ($scope.url.indexOf('/slide') == 0) {
+      $scope.subMenuItems = [
+        {
+          title: 'Oversigt',
+          path: '/#/slides',
+          classSuffix: 'overview'
+        },
+        {
+          title: 'Opret slide',
+          path: '/#/slide',
+          classSuffix: 'create-channel'
+        }
+      ];
+    }
+    else if ($scope.url.indexOf('/screen') == 0) {
+      $scope.subMenuItems = [
+        {
+          title: 'Oversigt',
+          path: '/#/screens',
+          classSuffix: 'overview'
+        },
+        {
+          title: 'Opret skærm',
+          path: '/#/screen',
+          classSuffix: 'create-channel'
+        }
+      ];
+    }
+    else if ($scope.url.indexOf('/template') == 0) {
+      $scope.subMenuItems = [
+        {
+          title: 'Oversigt',
+          path: '/#/templates',
+          classSuffix: 'overview'
+        },
+        {
+          title: 'Opret template',
+          path: '/#/template',
+          classSuffix: 'create-channel'
+        }
+      ];
+    }
+    else {
+      $scope.subMenuItems = [];
+    }
+  }
+  updateSubMenu();
 
   // Navigation menu open / close
   $scope.toggleNavMenu = function(){
@@ -12,5 +76,6 @@ ikApp.controller('MenuController', function($scope, $rootScope, $location) {
 
   $rootScope.$on('$locationChangeSuccess', function(){
     $scope.url = $location.url();
+    updateSubMenu();
   });
 });
