bcbc8773c38eaebfc5a29889b03757b9aad2f0d1
Troels Ugilt Jensen <tujensen@gmail.com>
2014-06-26 08:44:10 +0200

    Fixed channel directive's run function.

--- web/js/directives/channelDirectives.js
+++ web/js/directives/channelDirectives.js
@@ -7,29 +7,36 @@ ikApp.directive('ikChannel', ['$interval', 'channelFactory', 'slideFactory', fun
     },
     link: function(scope, element, attrs) {
       scope.slideIndex = 0;
+      scope.channel = {};
+      scope.slides = [];
+      scope.templateURL = '';
+      scope.playText = '';
 
-      scope.setTemplate = function(slideID) {
-        console.log(scope.channel.slides);
-        slideFactory.getSlide(scope.channel.slides[slideID]).then(function(data) {
-          scope.ikSlide = data;
-          scope.templateURL = '/ik-templates/' + scope.ikSlide.template + '/' + scope.ikSlide.template + '.html';
+      scope.setTemplate = function() {
+        scope.ikSlide = scope.slides[scope.slideIndex];
+        scope.templateURL = '/ik-templates/' + scope.ikSlide.template + '/' + scope.ikSlide.template + '.html';
 
-          scope.theStyle = {
-            width: "" + scope.ikWidth + "px",
-            height: "" + parseFloat(scope.ikSlide.options.idealdimensions.height * parseFloat(scope.ikWidth / scope.ikSlide.options.idealdimensions.width)) + "px",
-            fontsize: "" + parseFloat(scope.ikSlide.options.fontsize * parseFloat(scope.ikWidth / scope.ikSlide.options.idealdimensions.width)) + "px"
-          }
-        });
+        scope.theStyle = {
+          width: "" + scope.ikWidth + "px",
+          height: "" + parseFloat(scope.ikSlide.options.idealdimensions.height * parseFloat(scope.ikWidth / scope.ikSlide.options.idealdimensions.width)) + "px",
+          fontsize: "" + parseFloat(scope.ikSlide.options.fontsize * parseFloat(scope.ikWidth / scope.ikSlide.options.idealdimensions.width)) + "px"
+        }
       }
 
       attrs.$observe('ikId', function(val) {
-        scope.templateURL = '';
-
-        scope.channel = [];
         channelFactory.getChannel(val).then(function(data) {
           scope.channel = data;
-
-          scope.setTemplate(scope.channel.slides[0]);
+          angular.forEach(scope.channel.slides, function(value, key) {
+            slideFactory.getSlide(value).then(function(data) {
+              if (data != []) {
+                scope.slides.push(data);
+                if (key === 0) {
+                  scope.setTemplate();
+                  scope.playText = 'Play';
+                }
+              }
+            });
+          });
         });
       });
 
@@ -37,15 +44,17 @@ ikApp.directive('ikChannel', ['$interval', 'channelFactory', 'slideFactory', fun
         if (angular.isDefined(scope.interval)) {
           $interval.cancel(scope.interval);
           scope.interval = undefined;
+          scope.playText = 'Play';
+        } else {
+          scope.interval = $interval(function() {
+            scope.setTemplate();
+
+            scope.slideIndex = (scope.slideIndex + 1) % scope.slides.length;
+          }, 5000);
+          scope.playText = 'Stop';
         }
-
-        scope.interval = $interval(function() {
-          scope.setTemplate(scope.channel.slides[scope.slideIndex]);
-
-          scope.slideIndex = (scope.slideIndex + 1) % scope.channel.slides.length;
-        }, 10000);
       }
     },
-    template: '<div data-ng-include="" src="templateURL"></div><div class="play" ng-click="play()">PLAY</div>'
+    template: '<div data-ng-include="" src="templateURL"></div><div class="play" ng-click="play()">{{playText}}</div>'
   }
 }]);
