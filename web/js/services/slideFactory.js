0d6d1e4094334ea7e9853a36c57def45deb0ed73
Troels Ugilt Jensen <tujensen@gmail.com>
2014-07-02 14:56:05 +0200

    Fixed issue with background-size. Moved default options to templateFactory.

--- web/js/services/slideFactory.js
+++ web/js/services/slideFactory.js
@@ -34,15 +34,21 @@ ikApp.factory('slideFactory', ['$http', '$q', 'userFactory', function($http, $q,
 
     if (id === null || id === undefined || id === '') {
       defer.resolve(currentSlide);
-    } else {
-      $http.get('/api/slide/' + id)
-        .success(function(data, status) {
-          currentSlide = data;
-          defer.resolve(currentSlide);
-        })
-        .error(function(data, status) {
-          defer.reject(status);
-        });
+    }
+    else {
+      if (currentSlide !== null && currentSlide.id == id) {
+        defer.resolve(currentSlide);
+      }
+      else {
+        $http.get('/api/slide/' + id)
+          .success(function(data, status) {
+            currentSlide = data;
+            defer.resolve(currentSlide);
+          })
+          .error(function(data, status) {
+            defer.reject(status);
+          });
+      }
     }
 
     return defer.promise;
@@ -107,15 +113,7 @@ ikApp.factory('slideFactory', ['$http', '$q', 'userFactory', function($http, $q,
       orientation: '',
       template: '',
       created: parseInt((new Date().getTime()) / 1000),
-      options: {
-        fontsize: '32',
-        bgcolor: '#ccc',
-        textcolor: '#fff',
-        textbgcolor: 'rgba(0, 0, 0, 0.7)',
-        image: '',
-        headline: '',
-        text: ''
-      }
+      options: null
     };
 
     return currentSlide;
