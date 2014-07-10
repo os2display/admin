654ed34940a9147841578ff2647f4785952997a8
Jesper Kristensen <cableman@linuxdev.dk>
2014-07-10 17:56:49 +0200

    Issue #7940 by cableman: Cleaned up the search factory

--- web/js/services/searchFactory.js
+++ web/js/services/searchFactory.js
@@ -1,16 +1,18 @@
 /**
- * Search factory.
+ * @file
+ * Search factory that handles communication with search engine.
+ *
+ * The communication is based on web-sockets via socket.io library.
  */
 ikApp.service('searchFactory', ['$q', '$rootScope', function($q, $rootScope) {
   var socket;
   var self = this;
 
-
   /**
    * Connect to the web-socket.
    *
-   * @param string token
-   *   JWT authentication token from the activation request.
+   * @param deferred
+   *   The is a deferred object that should be resovled on connection.
    */
   function getSocket(deferred) {
     // Get connected to the server.
@@ -28,15 +30,18 @@ ikApp.service('searchFactory', ['$q', '$rootScope', function($q, $rootScope) {
 
     // Handle disconnect event (fires when disconnected or connection fails).
     socket.on('disconnect', function (reason) {
-      if (reason === 'booted') {
-        // Reload application.
-        // TODO: Do a connect reload.
-      }
+      // @todo: re-connection is automatically handled by socket.io library,
+      // but we might need to stop sending request until reconnection or the
+      // request will be queued and send all at once... which could give some
+      // strange side effects in the application if not handled.
     });
   }
 
   /**
-   * Create the connection to the server with promise.
+   * Create the connection to the server.
+   *
+   * @return {promise}
+   *   An promise is return that will be resolved on connection.
    */
   function connect() {
     // Try to connect to the server if not already connected.
@@ -52,23 +57,34 @@ ikApp.service('searchFactory', ['$q', '$rootScope', function($q, $rootScope) {
     return deferred.promise;
   }
 
+  /**
+   * Send search request to the engine.
+   *
+   * The default search should have this form:
+   *
+   * {
+   *   "fields": 'title',
+   *     "text": '',
+   *     "sort": {
+   *       "created_at" : {
+   *         "order": "desc"
+   *       }
+   *     }
+   *   }
+   * }
+   *
+   * @param search
+   *   This is a json object as described above as default.
+   *
+   * @returns {promise}
+   *   When data is received from the backend. If no data found an empty JSON
+   *   object is retuned.
+   */
   this.search = function(search) {
     var deferred = $q.defer();
 
     connect().then(function () {
       socket.emit('search', search);
-      socket.on('result', function (data) {
-        deferred.resolve(data);
-      });
-    });
-
-    return deferred.promise;
-  };
-
-  this.latest = function(search) {
-    var deferred = $q.defer();
-    connect().then(function () {
-      socket.emit('search', { text: '', sort: 'created', type: search.type, app_id: search.app_id });
       socket.on('result', function (data) {
         deferred.resolve(data);
       });
