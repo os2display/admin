6cef3de51c215b93f2fb379f6d46d9e8b526eb0e
Ture Gjørup <ture@ignatz.dk>
2014-08-04 14:00:12 +0200

    Bugfix by turegjorup: Call to the parent's constructor with parent::__construct() added

--- src/Application/Sonata/UserBundle/Entity/User.php
+++ src/Application/Sonata/UserBundle/Entity/User.php
@@ -47,6 +47,7 @@ class User extends BaseUser
      */
     public function __construct()
     {
+        parent::__construct();
         $this->groups = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
