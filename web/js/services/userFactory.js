a560641dc2060829d676db580cfcd8055092cb28
Troels Ugilt Jensen <tujensen@gmail.com>
2014-09-10 16:06:45 +0200

    Issue #8229 by tuj: Added documentation

--- web/js/services/userFactory.js
+++ web/js/services/userFactory.js
@@ -1,25 +1,32 @@
 /**
+ * @file
+ * Contains the user factory.
+ */
+
+/**
  * User factory.
  */
-ikApp.factory('userFactory', ['$http', '$q', function($http, $q) {
-  var factory = {};
+ikApp.factory('userFactory', ['$http', '$q',
+  function($http, $q) {
+    var factory = {};
 
-  /**
-   * Get current user.
-   */
-  factory.getCurrentUser = function() {
-    var defer = $q.defer();
+    /**
+     * Get current user.
+     */
+    factory.getCurrentUser = function() {
+      var defer = $q.defer();
 
-    $http.get('/api/user')
-      .success(function(data) {
-        defer.resolve(data);
-      })
-      .error(function() {
-        defer.reject();
-      });
+      $http.get('/api/user')
+        .success(function(data) {
+          defer.resolve(data);
+        })
+        .error(function() {
+          defer.reject();
+        });
 
-    return defer.promise;
+      return defer.promise;
+    }
+
+    return factory;
   }
-
-  return factory;
-}]);
+]);
