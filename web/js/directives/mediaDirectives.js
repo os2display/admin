3cacbf164eb0a7f06f21e93add760dc107f95a1a
Troels Ugilt Jensen <tujensen@gmail.com>
2014-09-12 13:53:02 +0200

    Issue #8201 by tuj: Encapsulated media upload in directive.

--- web/js/directives/mediaDirectives.js
+++ web/js/directives/mediaDirectives.js
@@ -4,7 +4,12 @@
  */
 
 /**
+ * Media Overview Directive.
+ *
  * Directive to insert a media overview.
+ *
+ * Emits the mediaOverview.selectMedia event when a media from the overview has been clicked.
+ *
  * @param media-type
  *   which media type should be shown, "image" or "video",
  *   leave out show all media.
@@ -29,6 +34,20 @@ ikApp.directive('ikMediaOverview', function() {
           }
         }
       };
+
+      // Mouse hover on image.
+      $scope.hovering = false;
+
+      /**
+       * Adds hover overlay on media elements.
+       */
+      $scope.mouseHover = function(state) {
+        if(state > 0) {
+          $scope.hovering = state;
+        } else {
+          $scope.hovering = false;
+        }
+      }
 
       /**
        * Updates the images array by sending a search request.
@@ -106,6 +125,134 @@ ikApp.directive('ikMediaOverview', function() {
 });
 
 /**
+ * Media Upload Directive.
+ *
+ * Emits the mediaUpload.uploadSuccess event when upload i successful.
+ *
+ * Emits the 'mediaUpload.uploadComplete' event for a parent controller to catch.
+ *   Catch this event to handle when the upload is complete.
+ */
+ikApp.directive('ikMediaUpload', function() {
+  return {
+    restrict: 'E',
+    scope: {},
+    controller: function($scope, FileUploader) {
+      $scope.currentStep = 1;
+      $scope.uploadComplete = false;
+      $scope.uploadErrors = false;
+      $scope.uploadErrorText = '';
+
+      // Create an uploader
+      $scope.uploader = new FileUploader({
+        url: '/api/media',
+        filters: [{
+          name: 'imageFilter',
+          fn: function(item /*{File|FileLikeObject}*/, options) {
+            var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+            return '|jpg|png|jpeg|bmp|gif|mp4|'.indexOf(type) !== -1;
+          }
+        }]
+      });
+
+      /**
+       * Calls the hidden select files button.
+       */
+      $scope.selectFiles = function() {
+        angular.element( document.querySelector( '#select-files' )).click();
+      };
+
+      /**
+       * Clear the uploader queue.
+       */
+      $scope.clearQueue = function() {
+        $scope.uploader.clearQueue();
+        $scope.uploadComplete = false;
+        $scope.uploadErrors = false;
+        $scope.currentStep = 1;
+      }
+
+      /**
+       * Remove item from the uploader queue.
+       * @param item
+       */
+      $scope.removeItem = function(item) {
+        item.remove();
+        if ($scope.uploader.queue.length <= 0) {
+          $scope.currentStep = 1;
+          $scope.uploadComplete = false;
+          $scope.uploadErrors = false;
+        }
+      }
+
+      /**
+       * Checks whether the item is an image.
+       */
+      $scope.isImage = function(item) {
+        var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+        return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+      };
+
+      /**
+       * After adding a file to the upload queue, add an empty title to the file item.
+       */
+      $scope.uploader.onAfterAddingFile = function(item) {
+        item.formData = [{title: ''}];
+      };
+
+      /**
+       * After adding all files, increase current step.
+       * @param item
+       */
+      $scope.uploader.onAfterAddingAll = function() {
+        $scope.currentStep++;
+      };
+
+      /**
+       * If an error occurs.
+       * @param item
+       * @param response
+       * @param status
+       * @param headers
+       */
+      $scope.uploader.onErrorItem = function(item, response, status, headers) {
+        $scope.uploadErrors = true;
+
+        if (status === 413) {
+          $scope.uploadErrorText = "Billedet var for stort (fejlkode: 413)";
+        } else {
+          $scope.uploadErrorText = "Der skete en fejl (fejlkode: " + status + ")";
+        }
+      };
+
+      /**
+       * When all uploads are complete.
+       */
+      $scope.uploader.onCompleteAll = function() {
+        $scope.uploadComplete = true;
+      };
+
+      /**
+       * When an item has been uploaded successfully.
+       * @param item
+       * @param response
+       * @param status
+       * @param headers
+       */
+      $scope.uploader.onSuccessItem = function(item, response, status, headers) {
+        $scope.$emit('mediaUpload.uploadSuccess', {
+          image: item,
+          id: response[0],
+          queue: $scope.uploader.queue
+        });
+      };
+    },
+    link: function(scope, element, attrs) {
+    },
+    templateUrl: 'partials/directives/media-upload.html'
+  }
+});
+
+/**
  * The ng-thumb directive
  * NB! Has been renamed to ik-thumb
  * @author: nerv
@@ -130,7 +277,6 @@ ikApp.directive('ikThumb', ['$window', function($window) {
       if (!helper.support) return;
 
       var params = scope.$eval(attributes.ikThumb);
-      console.log(params);
 
       if (!helper.isFile(params.file)) {
         console.log("Not a file");
