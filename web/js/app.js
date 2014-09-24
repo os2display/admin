16d4425449c5e64fe10da3cb56d4fe670549140b
martinyde <yde001@gmail.com>
2014-09-24 15:30:02 +0200

    Martin Yde - Added angular placeholder for ie8-9

--- web/js/app.js
+++ web/js/app.js
@@ -7,4 +7,4 @@
  * ikApp - Main entry point for the app.
  * @type {*}
  */
-var ikApp = angular.module('ikApp', ['ngRoute', 'ngAnimate', 'angularFileUpload', 'checklist-model', 'colorpicker.module', 'datetimePicker'], function() {});
+var ikApp = angular.module('ikApp', ['ngRoute', 'ngAnimate', 'angularFileUpload', 'checklist-model', 'colorpicker.module', 'datetimePicker', 'taiPlaceholder'], function() {});
