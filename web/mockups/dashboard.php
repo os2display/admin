e2eeecc4d59905ecb1a5cff4dce30900487e7e1d
Troels Ugilt Jensen <tujensen@gmail.com>
2014-09-25 11:22:05 +0200

    Removed mockups.

--- web/mockups/dashboard.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <?php include 'inc/head.inc'; ?>
-  <header class="header" role="banner">
-    <div class="header--inner">
-      <?php include 'inc/header-main.inc'; ?>
-    </div>
-  </header>
-</html>
