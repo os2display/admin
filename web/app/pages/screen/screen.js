a5e859c6ab217e2b268e710451622ed402a49bf0
Troels Ugilt Jensen <tujensen@gmail.com>
2015-02-18 08:21:53 +0100

    Issue #KOBAONE-86 by tuj: Split templates html to own files.

--- web/app/pages/screen/screen.js
+++ web/app/pages/screen/screen.js
@@ -10,26 +10,31 @@ angular.module('ikApp').controller('ScreenController', ['$scope', '$location', '
   function($scope, $location, $routeParams, screenFactory, channelFactory, sharedChannelFactory, configuration) {
     $scope.sharingEnabled = configuration.sharingService.enabled;
     $scope.screen = {};
-    $scope.screenEditorTemplate = '';
     $scope.toolbarTemplate = null;
-    $scope.editRegion = null;
+    $scope.displayToolbar = false;
+    $scope.region = null;
 
     /**
      * Constructor.
      * Handles different settings of route parameters.
      */
     function init() {
+      console.log("init");
+
       if (!$routeParams.id) {
         // If the ID is not set, get an empty slide.
         $scope.screen = screenFactory.emptyScreen();
       } else {
         if ($routeParams.id === null || $routeParams.id === undefined || $routeParams.id === '') {
-          $location.path('/screen');
+          $location.path('/screen-overview');
         } else {
           // Get the screen from the backend.
-          screenFactory.getEditScreen($routeParams.id).then(
+          screenFactory.getScreen($routeParams.id).then(
             function(data) {
               $scope.screen = data;
+
+              // @TODO: Fix the default, so the template defaulted to full-screen. Then remove this!
+              $scope.screen.template = 'templates/screens/five-sections/five-sections-edit.html';
 
               $scope.screen.shared_channels.forEach(function(element) {
                 element.content = JSON.parse(element.content);
@@ -38,6 +43,12 @@ angular.module('ikApp').controller('ScreenController', ['$scope', '$location', '
               if ($scope.screen === {}) {
                 $location.path('/screen');
               }
+
+              console.log($scope.screen);
+            },
+            // Error getting
+            function(reason) {
+              console.log(reason);
             }
           );
         }
@@ -63,7 +74,8 @@ angular.module('ikApp').controller('ScreenController', ['$scope', '$location', '
 
     $scope.triggerTool = function triggerTool(tool) {
       $scope.toolbarTemplate = 'app/shared/toolbars/' + tool.name + '.html';
-      $scope.editRegion = tool.region;
+      $scope.region = tool.region;
+      $scope.displayToolbar = true;
     }
   }
 ]);
