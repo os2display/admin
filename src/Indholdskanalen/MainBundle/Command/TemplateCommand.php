8c937e7ed947b1b0bc35029b07c9d8f2b512e8e0
Troels Ugilt Jensen <tujensen@gmail.com>
2015-03-04 12:46:46 +0100

    Issue #KOBAONE-166 by tuj: Fixed scrutinizer-raised issues.

--- src/Indholdskanalen/MainBundle/Command/TemplateCommand.php
+++ src/Indholdskanalen/MainBundle/Command/TemplateCommand.php
@@ -28,7 +28,7 @@ class TemplateCommand extends ContainerAwareCommand {
   protected function configure() {
     $this
       ->setName('ik:templates:load')
-      ->setDescription("Load the templates from the disk into the database.");
+      ->setDescription('Load the templates from the disk into the database.');
   }
 
   /**
@@ -39,7 +39,7 @@ class TemplateCommand extends ContainerAwareCommand {
    * @return int|null|void
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $output->write("Loading templates to database... ");
+    $output->write('Loading templates to database... ');
 
     // Get database hooks.
     $container = $this->getContainer();
@@ -48,20 +48,21 @@ class TemplateCommand extends ContainerAwareCommand {
     $entityManager = $doctrine->getManager();
 
     // Get parameters.
-    $enabledTemplates = $container->getParameter("templates_screens_enabled");
-    $path = $container->get('kernel')->getRootDir() . '/../web/' . $container->getParameter("templates_screens_directory");
-    $serverAddress = $container->getParameter("absolute_path_to_server") . "/" . $container->getParameter("templates_screens_directory");;
+    $enabledTemplates = $container->getParameter('templates_screens_enabled');
+    $path = $container->get('kernel')
+        ->getRootDir() . '/../web/' . $container->getParameter('templates_screens_directory');
+    $serverAddress = $container->getParameter('absolute_path_to_server') . '/' . $container->getParameter('templates_screens_directory');
 
     // Iterate through templates directory (configurable).
     if ($handle = opendir($path)) {
-      while (false !== ($entry = readdir($handle))) {
-        if (is_dir($path . "/" . $entry) && $entry !== '.' && $entry !== '..') {
+      while (FALSE !== ($entry = readdir($handle))) {
+        if (is_dir($path . '/' . $entry) && $entry !== '.' && $entry !== '..') {
           if (!in_array($entry, $enabledTemplates)) {
             continue;
           }
 
           // Read .json for template
-          $str = file_get_contents($path . $entry . '/' . $entry . ".json");
+          $str = file_get_contents($path . $entry . '/' . $entry . '.json');
           $obj = json_decode($str);
 
           $template = $templateRepository->findOneById($obj->id);
@@ -89,6 +90,6 @@ class TemplateCommand extends ContainerAwareCommand {
       closedir($handle);
     }
 
-    $output->writeln("Done!");
+    $output->writeln('Done!');
   }
 }
