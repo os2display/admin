033f5d716a1c4b457f7afe1c1f22b9bb3f1192b7
Jesper Kristensen <cableman@linuxdev.dk>
2015-04-08 13:38:55 +0200

    Issue KOBA-131 by cableman: Added sentry to angular

--- web/app/app.js
+++ web/app/app.js
@@ -32,4 +32,23 @@ angular.module('ikApp', [
     'itkDateComponent'
   ],
   function() {}
-);
+).config(function($provide) {
+    'use strict';
+
+    // Install raven.
+    Raven.config('https://de9fc538a66e44c4a793fed8e5d39878@app.getsentry.com/41300', {
+      // pass along the version of your application
+      release: '3.0.0-aplha'
+
+      // we highly recommend restricting exceptions to a domain in order to filter out clutter
+      //whitelistUrls: ['example.com/scripts/']
+    }).install();
+
+
+    $provide.decorator("$exceptionHandler", ['$delegate', function ($delegate) {
+      return function (exception, cause) {
+        $delegate(exception, cause);
+        Raven.captureException(exception);
+      };
+    }]);
+  });
