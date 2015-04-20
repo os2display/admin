d5a68310fedb9868bfd274d852d25e63df435dd8
Troels Ugilt Jensen <tujensen@gmail.com>
2015-04-20 11:29:11 +0200

    Issue #KOBAONE-213 by tuj: Removed width/height/orientation from screen.

--- src/Indholdskanalen/MainBundle/Controller/ScreenController.php
+++ src/Indholdskanalen/MainBundle/Controller/ScreenController.php
@@ -87,15 +87,6 @@ class ScreenController extends Controller {
     if (isset($post->title)) {
       $screen->setTitle($post->title);
     }
-    if (isset($post->orientation)) {
-      $screen->setOrientation($post->orientation);
-    }
-    if (isset($post->width)) {
-      $screen->setWidth($post->width);
-    }
-    if (isset($post->height)) {
-      $screen->setHeight($post->height);
-    }
     if (isset($post->description)) {
       $screen->setDescription($post->description);
     }
