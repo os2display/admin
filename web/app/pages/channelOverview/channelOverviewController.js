ed22acd247ec2de634884cf1ae9df48e9fd03eb3
Troels Ugilt Jensen <tujensen@gmail.com>
2015-04-23 15:39:25 +0200

    Issue #KOBAONE-217 by tuj: Added error messages to all pages in the backend.

--- web/app/pages/channelOverview/channelOverviewController.js
+++ web/app/pages/channelOverview/channelOverviewController.js
@@ -6,8 +6,8 @@
 /**
  * Channels controller handles the display and selection of channels.
  */
-angular.module('ikApp').controller('ChannelOverviewController', ['$scope', 'sharedChannelFactory', 'channelFactory', 'configuration',
-  function($scope, sharedChannelFactory, channelFactory, configuration) {
+angular.module('ikApp').controller('ChannelOverviewController', ['$scope', 'sharedChannelFactory', 'channelFactory', 'configuration', 'itkLogFactory',
+  function($scope, sharedChannelFactory, channelFactory, configuration, itkLogFactory) {
     'use strict';
 
     $scope.shareDialogShow = false;
@@ -20,24 +20,36 @@ angular.module('ikApp').controller('ChannelOverviewController', ['$scope', 'shar
         $scope.shareDialogChannel = channel;
 
         channelFactory.getChannel(channel.id).then(
-          function(data) {
+          function success(data) {
             $scope.shareDialogChannel = data;
             if (!$scope.shareDialogChannel.sharing_indexes) {
               $scope.shareDialogChannel.sharing_indexes = [];
             }
+          },
+          function error(reason) {
+            itkLogFactory.error('Hentning af kanal fejlede', reason);
           }
         );
       });
 
       $scope.sharingIndexes = [];
-      sharedChannelFactory.getSharingIndexes().then(function(data) {
-        $scope.sharingIndexes = data;
-      });
+      sharedChannelFactory.getSharingIndexes().then(
+        function success(data) {
+          $scope.sharingIndexes = data;
+        },
+        function error(reason) {
+          itkLogFactory.error('Hentning af delingsindeks fejlede.', reason);
+        }
+      );
 
       $scope.saveSharingChannel = function saveSharingChannel() {
         channelFactory.channelShare($scope.shareDialogChannel).then(
-          function(data) {},
-          function(reason) {}
+          function() {
+            itkLogFactory.info('Delingskonfiguration af kanal lykkedes.', 3000);
+          },
+          function(reason) {
+            itkLogFactory.error('Deling af kanal fejlede.', reason);
+          }
         );
       };
     }
