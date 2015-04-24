8c76aa8e06ecdf41a97533839049ceec26c46fca
Troels Ugilt Jensen <tujensen@gmail.com>
2015-04-24 10:19:45 +0200

    Issue #KOBAONE-226 by tuj: Added whitelist to backend to allow sharing between installations.

--- web/app/app.js
+++ web/app/app.js
@@ -35,7 +35,21 @@ angular.module('ikApp', [
   ],
   function () {
   }
-).config(function ($provide) {
+)
+  .config(function ($sceDelegateProvider) {
+    'use strict';
+
+    // The administration interface and the client code do not run on the same
+    // domain/sub-domain hence we need to whitelist the domains to load slide
+    // templates and CSS form the administration domain.
+    $sceDelegateProvider.resourceUrlWhitelist([
+      // Allow same origin resource loads.
+      'self',
+      // Allow loading from outer templates domain.
+      '**'
+    ]);
+  })
+  .config(function ($provide) {
     "use strict";
 
     $provide.decorator("$exceptionHandler", ['$delegate', '$injector', function ($delegate, $injector) {
