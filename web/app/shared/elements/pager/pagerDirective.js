77661020acff576d97478c23d4da19da10519474
Troels Ugilt Jensen <tujensen@gmail.com>
2015-09-17 10:30:47 +0200

    Issue #TEAMMANAGE-180 by tuj: Fixed pager

--- web/app/shared/elements/pager/pagerDirective.js
+++ web/app/shared/elements/pager/pagerDirective.js
@@ -15,6 +15,8 @@ angular.module('ikApp').directive('ikPager', [
       replace: true,
       scope: true,
       controller: function ($scope) {
+        $scope.pagesFrom = 0;
+        $scope.pagesPerLine = 10;
 
         /**
          * Click handler to change page.
@@ -24,12 +26,18 @@ angular.module('ikApp').directive('ikPager', [
         $scope.changePage = function changePage(page) {
           $scope.pager.page = page;
           $scope.updateSearch();
+
+          $scope.pagesFrom = Math.floor($scope.pager.page / $scope.pagesPerLine) * $scope.pagesPerLine;
         };
 
         $scope.prevPage = function prevPage() {
           if ($scope.pager.page > 0) {
             $scope.pager.page--;
             $scope.updateSearch();
+          }
+
+          if ($scope.pager.page < $scope.pagesFrom) {
+            $scope.pagesFrom = $scope.pagesFrom - $scope.pagesPerLine;
           }
         };
 
@@ -38,6 +46,34 @@ angular.module('ikApp').directive('ikPager', [
             $scope.pager.page++;
             $scope.updateSearch();
           }
+
+          if ($scope.pager.page === $scope.pagesFrom + $scope.pagesPerLine) {
+            $scope.pagesFrom = $scope.pagesFrom + $scope.pagesPerLine;
+          }
+        };
+
+        $scope.prevLine = function firstPage() {
+          if ($scope.pager.page > 0) {
+            $scope.pager.page = $scope.pager.page - $scope.pagesPerLine;
+            if ($scope.pager.page < 0) {
+              $scope.pager.page = 0;
+            }
+            $scope.updateSearch();
+          }
+
+          $scope.pagesFrom = Math.floor($scope.pager.page / $scope.pagesPerLine) * $scope.pagesPerLine;
+        };
+
+        $scope.nextLine = function lastPage() {
+          if ($scope.pager.page < $scope.pager.max - 1) {
+            $scope.pager.page = $scope.pager.page + $scope.pagesPerLine;
+            if ($scope.pager.page >= $scope.pager.max) {
+              $scope.pager.page = $scope.pager.max - 1;
+            }
+            $scope.updateSearch();
+          }
+
+          $scope.pagesFrom = Math.floor($scope.pager.page / $scope.pagesPerLine) * $scope.pagesPerLine;
         };
 
         // Keep an any on changes in number of hits.
