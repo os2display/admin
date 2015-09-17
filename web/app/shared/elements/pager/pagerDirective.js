02ea353f0096dee240aba32fa3b9e04c20d247fb
Troels Ugilt Jensen <tujensen@gmail.com>
2015-09-17 10:39:19 +0200

    Issue #TEAMMANAGE-180 by tuj: Fixed pager comments

--- web/app/shared/elements/pager/pagerDirective.js
+++ web/app/shared/elements/pager/pagerDirective.js
@@ -15,7 +15,9 @@ angular.module('ikApp').directive('ikPager', [
       replace: true,
       scope: true,
       controller: function ($scope) {
+        // The lowest page to show.
         $scope.pagesFrom = 0;
+        // The max number of results to show.
         $scope.pagesPerLine = 10;
 
         /**
@@ -30,6 +32,9 @@ angular.module('ikApp').directive('ikPager', [
           $scope.pagesFrom = Math.floor($scope.pager.page / $scope.pagesPerLine) * $scope.pagesPerLine;
         };
 
+        /**
+         * Move one page back.
+         */
         $scope.prevPage = function prevPage() {
           if ($scope.pager.page > 0) {
             $scope.pager.page--;
@@ -41,6 +46,9 @@ angular.module('ikApp').directive('ikPager', [
           }
         };
 
+        /**
+         * Move one page forward.
+         */
         $scope.nextPage = function nextPage() {
           if ($scope.pager.page < $scope.pager.max - 1) {
             $scope.pager.page++;
@@ -52,7 +60,10 @@ angular.module('ikApp').directive('ikPager', [
           }
         };
 
-        $scope.prevLine = function firstPage() {
+        /**
+         * Move $scope.pagesPerLine results back.
+         */
+        $scope.prevLine = function prevLine() {
           if ($scope.pager.page > 0) {
             $scope.pager.page = $scope.pager.page - $scope.pagesPerLine;
             if ($scope.pager.page < 0) {
@@ -64,7 +75,10 @@ angular.module('ikApp').directive('ikPager', [
           $scope.pagesFrom = Math.floor($scope.pager.page / $scope.pagesPerLine) * $scope.pagesPerLine;
         };
 
-        $scope.nextLine = function lastPage() {
+        /**
+         * Move $scope.pagesPerLine results forward.
+         */
+        $scope.nextLine = function nextLine() {
           if ($scope.pager.page < $scope.pager.max - 1) {
             $scope.pager.page = $scope.pager.page + $scope.pagesPerLine;
             if ($scope.pager.page >= $scope.pager.max) {
@@ -76,8 +90,8 @@ angular.module('ikApp').directive('ikPager', [
           $scope.pagesFrom = Math.floor($scope.pager.page / $scope.pagesPerLine) * $scope.pagesPerLine;
         };
 
-        // Keep an any on changes in number of hits.
-        $scope.$watch('hits', function (hits) {
+        // Keep a watch on changes in number of hits.
+        $scope.$watch('hits', function watchHits(hits) {
             var pages = [];
             $scope.pager.max = 0;
             if (hits > $scope.pager.size) {
