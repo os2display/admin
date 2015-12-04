0b9cd6ebb1900b35f14748c1665782ea3e2a93c5
Troels Ugilt Jensen <tujensen@gmail.com>
2015-12-04 08:56:24 +0100

    Issue #TEAMMANAGE-251 by tuj: Cleanup

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,59 +3,51 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
-{
-    public function registerBundles()
-    {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+class AppKernel extends Kernel {
+  public function registerBundles() {
+    $bundles = array(
+      new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+      new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+      new Symfony\Bundle\TwigBundle\TwigBundle(),
+      new Symfony\Bundle\MonologBundle\MonologBundle(),
+      new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+      new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+      new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+      new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+      // FOS.
+      new FOS\UserBundle\FOSUserBundle(),
+      // Knp.
+      new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+      // Sonata.
+      new Sonata\CoreBundle\SonataCoreBundle(),
+      new Sonata\BlockBundle\SonataBlockBundle(),
+      new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+      new Sonata\AdminBundle\SonataAdminBundle(),
+      new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+      // User.
+      new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+      new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+      // Media.
+      new Sonata\MediaBundle\SonataMediaBundle(),
+      new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+      // Rss/Atom
+      new Debril\RssAtomBundle\DebrilRssAtomBundle(),
+      // Serializing.
+      new JMS\SerializerBundle\JMSSerializerBundle(),
+      // Indholdskanalen
+      new Indholdskanalen\MainBundle\IndholdskanalenMainBundle(),
+    );
 
-            // FOS.
-            new FOS\UserBundle\FOSUserBundle(),
-
-            // Knp.
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-
-            // Sonata.
-            new Sonata\CoreBundle\SonataCoreBundle(),
-            new Sonata\BlockBundle\SonataBlockBundle(),
-            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-            new Sonata\AdminBundle\SonataAdminBundle(),
-            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
-
-            // User.
-            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
-            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
-
-            // Media.
-            new Sonata\MediaBundle\SonataMediaBundle(),
-            new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
-
-            // Serializing.
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-          new Debril\RssAtomBundle\DebrilRssAtomBundle(),
-
-            new Indholdskanalen\MainBundle\IndholdskanalenMainBundle(),
-        );
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
-
-        return $bundles;
+    if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+      $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+      $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+      $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
-    }
+    return $bundles;
+  }
+
+  public function registerContainerConfiguration(LoaderInterface $loader) {
+    $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+  }
 }
