aae54cf4ae9a4fba00b1ee83dcaf8519cbe1cfe0
Jesper Kristensen <cableman@linuxdev.dk>
2016-06-03 10:52:30 +0200

    Issue #ARU-77: Create base controller for time-line app

--- web/apps/timelineApp/timelineChannelController.js
+++ web/apps/timelineApp/timelineChannelController.js
@@ -1,46 +1,12 @@
 /**
  * @file
- * @TODO: Change to use search function.
- * @TODO: Move all this search logic into a service on the BUS.
+ * Controller for the channel overview time-line.
  */
-angular.module('timelineApp').controller('TimelineChannelController', ['busService', 'timelineService', '$scope',
-  function (busService, timelineService, $scope) {
+angular.module('timelineApp').controller('TimelineChannelController', ['busService', '$scope', '$controller',
+  function (busService, $scope, $controller) {
     'use strict';
 
-    $scope.loading = false;
-
-    // Set default values.
-    $scope.sort = { "created_at": "desc" };
-
-    // Default pager values.
-    $scope.pager = {
-      "size": 6,
-      "page": 0
-    };
-    $scope.hits = 0;
-
-    // Screens to display.
-    $scope.screens = [];
-
-    // Setup default search options.
-    var search = {
-      "fields": [ 'title' ],
-      "text": '',
-      "sort": {
-        "created_at" : {
-          "order": "desc"
-        }
-      },
-      'pager': $scope.pager
-    };
-
-    timelineService.fetchData().then(
-      function success(data) {
-        $scope.data = data;
-      },
-      function error(reason) {
-        console.error(reason);
-      }
-    );
+    $scope.searchType = 'Indholdskanalen\\MainBundle\\Entity\\Channel';
+    $controller('TimelineBaseController', {$scope: $scope});
   }
 ]);
