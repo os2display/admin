e9aff1d8d54eab8d205b8bdfea1bc1f2934a4a61
Troels Ugilt Jensen <tujensen@gmail.com>
2016-06-27 09:54:01 +0200

    Issue #SUPPORT-649 by tuj: Changed to doctrine based migration

--- app/DoctrineMigrations/Version20160623082121.php
+++ app/DoctrineMigrations/Version20160623082121.php
@@ -4,42 +4,54 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Remove all instagram slides from the db.
  * Remove instagram template from db.
  */
-class Version20160623082121 extends AbstractMigration
-{
-    /**
-     * @param Schema $schema
-     */
-    public function up(Schema $schema)
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+class Version20160623082121 extends AbstractMigration implements ContainerAwareInterface {
+  private $container;
 
-        // Delete all Instagram slide occurrences in the ik_channelslideorder table.
-        $this->addSql('DELETE FROM ik_channelslideorder
-                                WHERE slide_id IN (
-                                      SELECT (id) FROM ik_slide WHERE slide_type=\'instagram\'
-                                      )
-                                                                
-        ');
+  public function setContainer(ContainerInterface $container = NULL) {
+    $this->container = $container;
+  }
 
-        // Delete all Instagram slides
-        $this->addSql('DELETE FROM ik_slide WHERE slide_type=\'instagram\'');
+  /**
+   * @param Schema $schema
+   */
+  public function up(Schema $schema) {
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->abortIf($this->connection->getDatabasePlatform()
+        ->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        // Remove instagram template from db.
-        $this->addSql('DELETE FROM ik_slide_templates WHERE id=\'dokk1-instagram\'');
+    $em = $this->container->get('doctrine')->getManager();
+
+    $slides = $this->container->get('doctrine')
+      ->getRepository('IndholdskanalenMainBundle:Slide')
+      ->findBy(array('slideType' => 'instagram'));
+
+    foreach ($slides as $slide) {
+      $em->remove($slide);
     }
 
-    /**
-     * @param Schema $schema
-     */
-    public function down(Schema $schema)
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    $slideTemplate = $this->container->get('doctrine')
+      ->getRepository('IndholdskanalenMainBundle:SlideTemplate')->find('dokk1-instagram');
+
+    if ($slideTemplate) {
+      $em->remove($slideTemplate);
     }
+
+    $em->flush();
+  }
+
+  /**
+   * @param Schema $schema
+   */
+  public function down(Schema $schema) {
+    // this down() migration is auto-generated, please modify it to your needs
+    $this->abortIf($this->connection->getDatabasePlatform()
+        ->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+  }
 }
