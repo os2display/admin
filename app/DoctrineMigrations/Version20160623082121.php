325ffedaf9653123dde8b5276882b14bc1f2cdd4
Jesper Kristensen <cableman@linuxdev.dk>
2016-06-27 11:17:29 +0200

    Issue #SUPPORT-649: Reviewed migration code to remove instagram slides

--- app/DoctrineMigrations/Version20160623082121.php
+++ app/DoctrineMigrations/Version20160623082121.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @file
+ * Update database tables in relation to instagram.
+ *
+ * 1. Remove all instagram slides from the db.
+ * 2. Remove instagram template from db.
+ */
 
 namespace Application\Migrations;
 
@@ -14,28 +21,38 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class Version20160623082121 extends AbstractMigration implements ContainerAwareInterface {
   private $container;
 
+  /**
+   * Implements container aware interface.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface|NULL $container
+   *    The container that is injected.
+   */
   public function setContainer(ContainerInterface $container = NULL) {
     $this->container = $container;
   }
 
   /**
+   * Up migration.
+   *
    * @param Schema $schema
    */
   public function up(Schema $schema) {
-    // this up() migration is auto-generated, please modify it to your needs
     $this->abortIf($this->connection->getDatabasePlatform()
         ->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+    // Find all instagram slides and remove them.
     $em = $this->container->get('doctrine')->getManager();
-
     $slides = $this->container->get('doctrine')
       ->getRepository('IndholdskanalenMainBundle:Slide')
       ->findBy(array('slideType' => 'instagram'));
 
     foreach ($slides as $slide) {
+      // This will automatically update the channels and all other places this
+      // slide is used.
       $em->remove($slide);
     }
 
+    // Remove all usage of the instagram templates.
     $slideTemplate = $this->container->get('doctrine')
       ->getRepository('IndholdskanalenMainBundle:SlideTemplate')->find('dokk1-instagram');
 
@@ -47,10 +64,11 @@ class Version20160623082121 extends AbstractMigration implements ContainerAwareI
   }
 
   /**
+   * Down migration.
+   *
    * @param Schema $schema
    */
   public function down(Schema $schema) {
-    // this down() migration is auto-generated, please modify it to your needs
     $this->abortIf($this->connection->getDatabasePlatform()
         ->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
   }
