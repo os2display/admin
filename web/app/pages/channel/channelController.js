6dee79e55714dfb24efe6bee1c64396c5f8e3db0
Troels Ugilt Jensen <tujensen@gmail.com>
2016-12-20 12:49:33 +0100

    Issue #DOKKWAY-150: Added sorting buttons

--- web/app/pages/channel/channelController.js
+++ web/app/pages/channel/channelController.js
@@ -251,6 +251,49 @@ angular.module('ikApp').controller('ChannelController', ['$scope', '$location', 
     };
 
     /**
+     * Shuffle an array.
+     *
+     * @param array
+     * @return {*}
+     */
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+
+    /**
+     * Sort the slides according to criteria.
+     *
+     * Defaults to alphabetic.
+     *
+     * @param sortCriteria
+     */
+    $scope.sortSlides = function sortSlides(sortCriteria) {
+      if (sortCriteria === 'random') {
+        $scope.channel.slides = shuffle($scope.channel.slides);
+        return;
+      }
+
+      var reverse = $scope.lastSortUsed === sortCriteria;
+
+      $scope.lastSortUsed = sortCriteria;
+
+      $scope.channel.slides = $filter('orderBy')($scope.channel.slides, reverse ? "-" : "" + sortCriteria);
+    };
+
+    /**
      * Change channel creation step.
      * @param step
      */
