c6fcbed5617cb7153aac00d5372455c2f30a0130
Troels Ugilt Jensen <tujensen@gmail.com>
2017-05-23 15:32:17 +0200

    Issue #OS2DISP-84: Fixed issue with running commands from cli

--- src/Indholdskanalen/MainBundle/IndholdskanalenMainBundle.php
+++ src/Indholdskanalen/MainBundle/IndholdskanalenMainBundle.php
@@ -17,9 +17,11 @@ class IndholdskanalenMainBundle extends Bundle {
   public function boot() {
     // Enable filter and inject container.
     // @see http://stackoverflow.com/a/14650403
-    $em = $this->container->get('doctrine.orm.default_entity_manager');
-    $conf = $em->getConfiguration();
-    $conf->addFilter('filter_grouping', GroupingFilter::class);
-    $em->getFilters()->enable('filter_grouping')->setContainer($this->container);
+    if (php_sapi_name() !== 'cli') {
+      $em = $this->container->get('doctrine.orm.default_entity_manager');
+      $conf = $em->getConfiguration();
+      $conf->addFilter('filter_grouping', GroupingFilter::class);
+      $em->getFilters()->enable('filter_grouping')->setContainer($this->container);
+    }
   }
 }
