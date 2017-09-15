909e7a2c8cf1363aaae8e34bef2da9c3d2d04a69
Troels Ugilt Jensen <tujensen@gmail.com>
2017-09-15 08:44:17 +0200

    Issue #SUPPORT-1201: Removed users from get all groups call

--- src/Indholdskanalen/MainBundle/Controller/GroupController.php
+++ src/Indholdskanalen/MainBundle/Controller/GroupController.php
@@ -24,151 +24,150 @@ use Symfony\Component\HttpFoundation\Request;
  * @Route("api/group")
  * @Rest\View(serializerGroups={"api"})
  */
-class GroupController extends ApiController {
-  protected static $editableProperties = ['title'];
+class GroupController extends ApiController
+{
+    protected static $editableProperties = ['title'];
 
-  /**
-   * Lists all group entities.
-   *
-   * @Rest\Get("", name="api_group_index")
-   * @ApiDoc(
-   *   section="Groups",
-   *   description="Get all groups",
-   *   tags={"group"}
-   * )
-   *
-   * @return \Symfony\Component\HttpFoundation\JsonResponse
-   */
-  public function indexAction() {
-    $groups = $this->findAll(Group::class);
-
-    foreach ($groups as $group) {
-      $group->buildUsers();
+    /**
+     * Lists all group entities.
+     *
+     * @Rest\Get("", name="api_group_index")
+     * @ApiDoc(
+     *   section="Groups",
+     *   description="Get all groups",
+     *   tags={"group"}
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function indexAction()
+    {
+        $groups = $this->findAll(Group::class);
+        return $this->setApiData($groups);
     }
 
-    return $this->setApiData($groups);
-  }
+    /**
+     * Creates a new group entity.
+     *
+     * @Rest\Post("", name="api_group_new")
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function newAction(Request $request)
+    {
+        $data = $this->getData($request);
 
-  /**
-   * Creates a new group entity.
-   *
-   * @Rest\Post("", name="api_group_new")
-   *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   * @return \Symfony\Component\HttpFoundation\JsonResponse
-   */
-  public function newAction(Request $request) {
-    $data = $this->getData($request);
+        try {
+            $group = $this->get('os2display.group_manager')->createGroup($data);
+        } catch (ValidationException $e) {
+            throw new HttpDataException(Codes::HTTP_BAD_REQUEST, $data, 'Invalid data', $e);
+        } catch (DuplicateEntityException $e) {
+            throw new HttpDataException(Codes::HTTP_CONFLICT, $data, 'Duplicate user', $e);
+        }
 
-    try {
-      $group = $this->get('os2display.group_manager')->createGroup($data);
-    }
-    catch (ValidationException $e) {
-      throw new HttpDataException(Codes::HTTP_BAD_REQUEST, $data, 'Invalid data', $e);
-    }
-    catch (DuplicateEntityException $e) {
-      throw new HttpDataException(Codes::HTTP_CONFLICT, $data, 'Duplicate user', $e);
-    }
-
-    // Send response.
-    return $this->createCreatedResponse($this->setApiData($group));
-  }
-
-  /**
-   * @Rest\Get("/roles")
-   * @ApiDoc(
-   *   section="Groups",
-   *   description="Get all available group roles"
-   * )
-   *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   * @return array
-   */
-  public function getRoles(Request $request) {
-    $translator = $this->get('translator');
-    $locale = $request->get('locale', $this->getParameter('locale'));
-
-    $roles = GroupRoles::getRoleNames();
-    $labels = array_map(function ($role) use ($translator, $locale) {
-      return $translator->trans($role, [], 'IndholdskanalenMainBundle', $locale);
-    }, $roles);
-    $data = array_combine($roles, $labels);
-    asort($data);
-
-    return $data;
-  }
-
-  /**
-   * Finds and displays a group entity.
-   *
-   * @Rest\Get("/{id}", name="api_group_show")
-   *
-   * @Security("is_granted('READ', group)")
-   *
-   * @param \Indholdskanalen\MainBundle\Entity\Group $group
-   * @return Group
-   */
-  public function showAction(Group $group) {
-    $group->buildUsers();
-
-    return $this->setApiData($group);
-  }
-
-  /**
-   * Displays a form to edit an existing group entity.
-   *
-   * @Rest\Put("/{id}", name="api_group_edit")
-   *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   * @param \Indholdskanalen\MainBundle\Entity\Group $group
-   * @return \Symfony\Component\HttpFoundation\JsonResponse
-   */
-  public function editAction(Request $request, Group $group) {
-    $data = $this->getData($request);
-
-    try {
-      $group = $this->get('os2display.group_manager')->updateGroup($group, $data);
-    }
-    catch (ValidationException $e) {
-      throw new HttpDataException(Codes::HTTP_BAD_REQUEST, $data, 'Invalid data', $e);
-    }
-    catch (DuplicateEntityException $e) {
-      throw new HttpDataException(Codes::HTTP_CONFLICT, $data, 'Duplicate user', $e);
+        // Send response.
+        return $this->createCreatedResponse($this->setApiData($group));
     }
 
-    return $this->setApiData($group);
-  }
+    /**
+     * @Rest\Get("/roles")
+     * @ApiDoc(
+     *   section="Groups",
+     *   description="Get all available group roles"
+     * )
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return array
+     */
+    public function getRoles(Request $request)
+    {
+        $translator = $this->get('translator');
+        $locale = $request->get('locale', $this->getParameter('locale'));
 
-  /**
-   * Deletes a group entity.
-   *
-   * @Rest\Delete("/{id}", name="api_group_delete")
-   *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   * @param \Indholdskanalen\MainBundle\Entity\Group $group
-   * @return \Symfony\Component\HttpFoundation\Response
-   */
-  public function deleteAction(Request $request, Group $group) {
-    $em = $this->getDoctrine()->getManager();
-    $em->remove($group);
-    $em->flush();
+        $roles = GroupRoles::getRoleNames();
+        $labels = array_map(function ($role) use ($translator, $locale) {
+            return $translator->trans($role, [], 'IndholdskanalenMainBundle', $locale);
+        }, $roles);
+        $data = array_combine($roles, $labels);
+        asort($data);
 
-    return $this->view(NULL, Codes::HTTP_NO_CONTENT);
-  }
-
-  /**
-   * Get users with roles in group.
-   *
-   * @Rest\Get("/{group}/users")
-   */
-  public function getGroupUsers(Group $group) {
-    $users = $group->buildUsers()->getUsers();
-
-    foreach ($users as $user) {
-      $user->buildGroupRoles($group);
+        return $data;
     }
 
-    return $this->setApiData($users);
-  }
+    /**
+     * Finds and displays a group entity.
+     *
+     * @Rest\Get("/{id}", name="api_group_show")
+     *
+     * @Security("is_granted('READ', group)")
+     *
+     * @param \Indholdskanalen\MainBundle\Entity\Group $group
+     * @return Group
+     */
+    public function showAction(Group $group)
+    {
+        $group->buildUsers();
+
+        return $this->setApiData($group);
+    }
+
+    /**
+     * Displays a form to edit an existing group entity.
+     *
+     * @Rest\Put("/{id}", name="api_group_edit")
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Indholdskanalen\MainBundle\Entity\Group $group
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function editAction(Request $request, Group $group)
+    {
+        $data = $this->getData($request);
+
+        try {
+            $group = $this->get('os2display.group_manager')->updateGroup($group, $data);
+        } catch (ValidationException $e) {
+            throw new HttpDataException(Codes::HTTP_BAD_REQUEST, $data, 'Invalid data', $e);
+        } catch (DuplicateEntityException $e) {
+            throw new HttpDataException(Codes::HTTP_CONFLICT, $data, 'Duplicate user', $e);
+        }
+
+        return $this->setApiData($group);
+    }
+
+    /**
+     * Deletes a group entity.
+     *
+     * @Rest\Delete("/{id}", name="api_group_delete")
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Indholdskanalen\MainBundle\Entity\Group $group
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAction(Request $request, Group $group)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($group);
+        $em->flush();
+
+        return $this->view(NULL, Codes::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Get users with roles in group.
+     *
+     * @Rest\Get("/{group}/users")
+     */
+    public function getGroupUsers(Group $group)
+    {
+        $users = $group->buildUsers()->getUsers();
+
+        foreach ($users as $user) {
+            $user->buildGroupRoles($group);
+        }
+
+        return $this->setApiData($users);
+    }
 
 }
