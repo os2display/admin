75abe3288614e60d107d3f2f0acfd52af2e4c163
Troels Ugilt Jensen <tujensen@gmail.com>
2017-10-02 14:44:12 +0200

    OS2DISP-113: Updated dependencies

--- setup_src.sh
+++
@@ -1,7 +0,0 @@
-git clone git@github.com:itk-os2display/default-template-bundle.git src/Os2Display/DefaultTemplateBundle
-git clone git@github.com:itk-os2display/exchange-bundle.git src/Os2Display/ExchangeBundle
-git clone git@github.com:itk-os2display/admin-bundle.git src/Os2Display/AdminBundle
-git clone git@github.com:itk-os2display/core-bundle.git src/Os2Display/CoreBundle
-git clone git@github.com:itk-os2display/media-bundle.git src/Os2Display/MediaBundle
-git clone git@github.com:itk-os2display/os2display-koba-integration.git src/Itk/KobaIntegrationBundle
-git clone git@github.com:itk-os2display/os2display-aarhus-templates.git src/Itk/AarhusTemplateBundle
---
+++ src/setup_src.sh
@@ -0,0 +1,7 @@
+git clone git@github.com:itk-os2display/default-template-bundle.git Os2Display/DefaultTemplateBundle
+git clone git@github.com:itk-os2display/exchange-bundle.git Os2Display/ExchangeBundle
+git clone git@github.com:itk-os2display/admin-bundle.git Os2Display/AdminBundle
+git clone git@github.com:itk-os2display/core-bundle.git Os2Display/CoreBundle
+git clone git@github.com:itk-os2display/media-bundle.git Os2Display/MediaBundle
+git clone git@github.com:itk-os2display/os2display-koba-integration.git Itk/KobaIntegrationBundle
+git clone git@github.com:itk-os2display/os2display-aarhus-templates.git Itk/AarhusTemplateBundle
