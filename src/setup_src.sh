7df8b04e3f29ad4a09a1f0a34af7002e92f4002c
Troels Ugilt Jensen <tujensen@gmail.com>
2017-10-13 19:36:42 +0200

    MKBDIGI-171: Fixed path

--- src/setup_src.sh
+++ src/setup_src.sh
@@ -3,7 +3,7 @@ git clone git@github.com:itk-os2display/exchange-bundle.git Os2Display/ExchangeB
 git clone git@github.com:itk-os2display/admin-bundle.git Os2Display/AdminBundle
 git clone git@github.com:itk-os2display/core-bundle.git Os2Display/CoreBundle
 git clone git@github.com:itk-os2display/media-bundle.git Os2Display/MediaBundle
-git clone git@github.com:itk-os2display/horizon-templates-bundle.git Os2Display/HorizonTemplateBundle
+git clone git@github.com:itk-os2display/horizon-template-bundle.git Os2Display/HorizonTemplateBundle
 
 git clone git@github.com:itk-os2display/os2display-koba-integration.git Itk/KobaIntegrationBundle
 git clone git@github.com:itk-os2display/os2display-aarhus-templates.git Itk/AarhusTemplateBundle
