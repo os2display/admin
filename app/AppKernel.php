09dddacff946ab6504aefc77e03f334a908faa52
Troels Ugilt Jensen <tujensen@gmail.com>
2017-10-13 10:29:48 +0200

    OS2DISP-113: Added HorizonTemplateBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,6 +39,7 @@ class AppKernel extends Kernel
             new Os2Display\DefaultTemplateBundle\Os2DisplayDefaultTemplateBundle(),
 
             new Os2Display\ExchangeBundle\Os2DisplayExchangeBundle(),
+            new Os2Display\HorizonTemplateBundle\Os2DisplayHorizonTemplateBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test', 'acceptance'))) {
