75fffb279814b5d7bd6169e4d7ff0d43c06608e6
Troels Ugilt Jensen <tujensen@gmail.com>
2017-10-30 14:45:54 +0100

    DRIFT-70: Added migrations to fix grouping after type name changes

--- app/DoctrineMigrations/Version20171030090153.php
+++ app/DoctrineMigrations/Version20171030090153.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20171024105325 extends AbstractMigration
+class Version20171030090153 extends AbstractMigration
 {
     /**
      * @param Schema $schema
