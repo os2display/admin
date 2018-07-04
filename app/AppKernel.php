2b975b668a13bc02bccdd4e85c6948ef0db2fed4
Troels Ugilt Jensen <tujensen@gmail.com>
2018-07-04 12:47:30 +0200

    OS2DISP-148: Removed itk bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,15 +36,6 @@ class AppKernel extends Kernel
             new Os2Display\AdminBundle\Os2DisplayAdminBundle(),
             new Os2Display\DefaultTemplateBundle\Os2DisplayDefaultTemplateBundle(),
 
-            new Itk\CampaignBundle\ItkCampaignBundle(),
-            new Itk\KobaIntegrationBundle\ItkKobaIntegrationBundle(),
-            new Itk\AarhusDataBundle\ItkAarhusDataBundle(),
-            new Itk\TemplateExtensionBundle\ItkTemplateExtensionBundle(),
-            new Itk\AarhusTemplateBundle\ItkAarhusTemplateBundle(),
-            new Itk\LokalcenterTemplateBundle\ItkLokalcenterTemplateBundle(),
-            new Itk\AarhusSecondTemplateBundle\ItkAarhusSecondTemplateBundle(),
-            new Itk\VimeoBundle\ItkVimeoBundle(),
-
             new AppBundle\AppBundle(),
         ];
 
