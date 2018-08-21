39aa56b838b0db41244e21d2939db48364b844fd
Lasse Borly <contact@lasseborly.dk>
2018-08-21 14:04:07 +0200

    add controller functions for the date picker

--- Resources/public/apps/dingEditors/event-date-picker.js
+++ Resources/public/apps/dingEditors/event-date-picker.js
@@ -1,26 +1,42 @@
-angular.module('datetimePicker').directive('datePicker', function() {
-  return {
-    restrict: 'A',
-    require: '^ngModel',
-    link: function(_, el) {
-      el.datetimepicker({
-        timepicker: false,
-        format: 'd.m.Y'
-      })
-    }
+angular.module('datetimePicker').directive('datePicker', () => ({
+  restrict: 'A',
+  require: '^ngModel',
+  link: (_, el) => {
+    el.datetimepicker({
+      timepicker: false,
+      format: 'd.m.Y'
+    })
   }
-})
+}))
 
-angular.module('toolsModule').directive('eventDatePicker', function() {
-  return {
-    restrict: 'E',
-    replace: true,
-    scope: {
-      slide: '=',
-      close: '&',
-      template: '@'
-    },
-    templateUrl:
-      '/bundles/kkbding2integration/apps/dingEditors/event-date-picker.html'
-  }
-})
+const dateDecorator = date => `Dato: ${date}`
+
+const headlineChange = scope => {
+  const options = scope.slide.options
+  const { text, from, to } = options.date
+
+  const setInfoHeader = (value = '') => options.infoheader = value
+
+  if (text) setInfoHeader(text)
+  else if (from && to) setInfoHeader(dateDecorator(`${from} - ${to}`))
+  else if (from) setInfoHeader(dateDecorator(from))
+  else setInfoHeader()
+}
+
+angular.module('toolsModule').directive('eventDatePicker', () => ({
+  restrict: 'E',
+  replace: true,
+  scope: {
+    slide: '=',
+    close: '&',
+    template: '@'
+  },
+  link: scope => {
+    const change = () => headlineChange(scope)
+    scope.onFromChange = change
+    scope.onToChange = change
+    scope.onTextChange = change
+  },
+  templateUrl:
+    '/bundles/kkbding2integration/apps/dingEditors/event-date-picker.html'
+}))
