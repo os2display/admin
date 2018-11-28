c0c40063a2f5caee0221345a5a68da85b36479da
Camilla Krag Jensen <ckj@reload.dk>
2018-11-28 10:34:24 +0100

    Clean data processing up a bit

--- src/kkos2-display-bundle/Resources/public/templates/js/kkeventsSlide.js
+++ src/kkos2-display-bundle/Resources/public/templates/js/kkeventsSlide.js
@@ -1,75 +1,121 @@
 /**
- * TODO
+ * Base slide, that just displays for slide.duration, then calls the callback.
  */
 
 // Register the function, if it does not already exist.
-if (!window.slideFunctions['kkevents']) {
-  console.log('jeg virker vist ikke endnu?')
-    window.slideFunctions['kkevents'] = {
-        /**
-         * Setup the slide for rendering.
-         * @param scope
-         *   The slide scope.
-         */
-        setup: function setupKkeventsSlide(scope) {
-            console.log('he');
-            var slide = scope.ikSlide;
+if (!window.slideFunctions['kk-events']) {
+  window.slideFunctions['kk-events'] = {
+    /**
+     * Setup the slide for rendering.
+     * @param scope
+     *   The slide scope.
+     */
+    setup: function setupKkEventsSlide(scope) {
+      var slide = scope.ikSlide;
+      if (!slide.external_data || !slide.external_data.events || slide.external_data.events.length < 1) {
+        return;
+      }
 
-            // Set currentLogo.
-            slide.currentLogo = slide.logo;
-            slide.currentImage = slide.media[0] && slide.media[0].image;
+      var events = slide.external_data.events;
 
-            // Setup the inline styling
-            scope.theStyle = {
-                width: "100%",
-                height: "100%",
-                fontsize: slide.options.fontsize * (scope.scale ? scope.scale : 1.0) + "px"
-            };
-
-            // Set the responsive fontsize if it is needed.
-            if (slide.options.responsive_fontsize) {
-                scope.theStyle.responsiveFontsize = slide.options.responsive_fontsize * (scope.scale ? scope.scale : 1.0) + "vw";
-            }
-        },
-
-        /**
-         * Run the slide.
-         *
-         * @param slide
-         *   The slide.
-         * @param region
-         *   The region to call when the slide has been executed.
-         */
-        run: function runEventSlide(slide, region) {
-            // Log slide-info before starting up.
-            region.itkLog.info("Running Event: " + slide.title);
-            if (slide.options.feed) {
-                if (slide.options.feed.library) {
-                    region.itkLog.info("- library nid: " + slide.options.feed.library);
-                }
-            }
-
-            // Get the strings we're going to display on the slide.
-            if (slide.external_data.intervalTexts) {
-                slide.intervalTexts = slide.external_data.intervalTexts;
-            }
-
-            if (slide.external_data.date_headline) {
-                slide.date_headline = slide.external_data.date_headline;
-            }
-
-            // Wait fadeTime before start to account for fade in.
-            var duration = slide.duration !== null ? slide.duration : 15;
-            region.$timeout(function () {
-                // Set the progress bar animation.
-                region.progressBar.start(duration);
-
-                // Wait for slide duration, then show next slide.
-                // + fadeTime to account for fade out.
-                region.$timeout(function () {
-                    region.nextSlide();
-                }, duration * 1000 + region.fadeTime);
-            }, region.fadeTime);
+      var currentSlide = -1;
+      var eventSlides = [];
+      events.forEach(function (event, i) {
+        if (i % 3 === 0) {
+          currentSlide++;
+          eventSlides[currentSlide] = [];
         }
-    };
+        eventSlides[currentSlide].push(event);
+      });
+
+
+      slide.event_settings = {
+        // Current slide being displayed, used by angular as index to find
+        // the slide
+        currentSlide: 0,
+        event_slides: eventSlides
+      };
+      console.log(slide);
+
+      // Setup the inline styling
+      scope.theStyle = {
+        width: "100%",
+        height: "100%",
+        fontsize: slide.options.fontsize * (scope.scale ? scope.scale : 1.0) + "px"
+      };
+
+      // // Set the responsive fontsize if it is needed.
+      // if (slide.options.responsive_fontsize) {
+      //   scope.theStyle.responsiveFontsize = slide.options.responsive_fontsize * (scope.scale ? scope.scale : 1.0) + "vw";
+      // }
+    },
+
+    /**
+     * Run the slide.
+     *
+     * @param slide
+     *   The slide.
+     * @param region
+     *   The region to call when the slide has been executed.
+     */
+    run: function runDingEventsSlide(slide, region) {
+      // Experience has shown that we can't be certain that all our data is
+      // present, so we'll have to be careful verify presence before accessing
+      // anything.
+      if (!slide.options || !slide.external_data || !slide.external_data.events || slide.external_data.events.length <= 0 || !slide.event_settings) {
+        // Go straight to the next slide if we're missing something. For now we
+        // simply assume that we have a "next" to go to, if not, we're going
+        // to loop real fast.
+
+        // In some situations the data is just about to be ready. Skipping the
+        // slide once and letting us get control back right away gives us the
+        // time we need.
+        if (!slide.loop_throttle) {
+          region.itkLog.info("Skipping to buy time for data ...");
+          slide.loop_throttle = 1;
+          return;
+        }
+
+        // We tried the skip, did not work, continue to next slide.
+        region.itkLog.info("No data for slide, skipping");
+
+        region.nextSlide();
+        return;
+      }
+      // Reset throttle in case we where successful.
+      slide.loop_throttle = false;
+
+      var slide_duration = slide.options.rss_duration ? slide.options.rss_duration : 15;
+
+      /**
+       * Iterate through event slides.
+       */
+      var dingEventTimeout = function () {
+        region.$timeout(function () {
+          // If we've reached the end, go to next (real) slide.
+          if (slide.event_settings.currentSlide + 1 >= slide.event_settings.event_slides.length) {
+            region.nextSlide();
+          } else {
+            // We have more, iterate to the next (event) slide.
+            slide.event_settings.currentSlide++;
+            dingEventTimeout();
+          }
+        }, slide_duration * 1000);
+      };
+
+      // reset slide-count.
+      slide.event_settings.currentSlide = 0;
+
+      // Trigger initial sleep an subsequent advance of slide.
+      dingEventTimeout();
+
+      // Wait fadeTime before start to account for fade in.
+      region.$timeout(function () {
+        // Set the progress bar animation.
+        var duration = slide_duration * slide.event_settings.event_slides.length;
+        region.progressBar.start(duration);
+      }, region.fadeTime);
+
+    }
+  };
 }
--- src/kkos2-display-bundle/Service/Kkos2DisplayService.php
+++ src/kkos2-display-bundle/Service/Kkos2DisplayService.php
@@ -1,92 +1,148 @@
 <?php
 /**
  * @file
- * Contains the DING2 service.
- *
- * Provides integration service with DING2.
+ * Service for Kultunaut-feeds for events.
  */
 
 namespace Kkos2\KkOs2DisplayIntegrationBundle\Service;
 
-
-use FOS\RestBundle\Decoder\XmlDecoder;
 use GuzzleHttp\Client;
-use Os2Display\CoreBundle\Entity\Slide;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use GuzzleHttp\Exception\TransferException;
 use Os2Display\CoreBundle\Events\CronEvent;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 class Kkos2DisplayService
 {
-
   /**
    * @var \Symfony\Bridge\Monolog\Logger $logger
    */
-//  private $logger;
-
+  private $logger;
+  /**
+   * @var \Symfony\Component\DependencyInjection\ContainerInterface $container
+   */
   private $container;
 
-  /**
-   * TODO
-   */
   public function __construct($container)
   {
     $this->container = $container;
-//    $this->logger = $this->container->get('logger');
-    error_log(get_class($this->container));
+    $this->logger = $this->container->get('logger');
   }
 
   /**
    * ik.onCron event listener.
    *
-   * Updates calendar slides.
-   *
    * @param CronEvent $event
    */
   public function onCron(CronEvent $event)
   {
-
-    $oldLocale = setlocale(LC_TIME, 0);
-    setlocale(LC_TIME, "da_DK.utf8");
-
-    // TODO. Not sure about the date stuff?
-
     $this->updateEvents();
-    setlocale(LC_TIME, $oldLocale);
   }
 
+  /**
+   * Fetch and parse Kultunaut feeds and save on slide external data.
+   */
   private function updateEvents()
   {
     $slideRepo = $this->container->get('doctrine')->getRepository('Os2DisplayCoreBundle:Slide');
+
     /** @var \Os2Display\CoreBundle\Entity\Slide[] $slides */
     $slides = $slideRepo->findBySlideType('kultunaut-event');
 
-    $normalizer = new GetSetMethodNormalizer();
-    $encoder = new XmlEncoder();
-    $serializer = new Serializer(array($normalizer), array($encoder));
-
     $client = new Client();
-
 
     foreach ($slides as $slide) {
       $options = $slide->getOptions();
-      if (!empty($options['source'])) {
-        $response = $client->get($options['source'], [
-          'headers' => [
-            'Accept' => 'application/xml'
-          ]
-        ]);
-        $body = $response->getBody();
-        // TODO. Error handling. Og genbrug af XML hvis det er samme feed.
-        $contents = (string) $body;
-        $xml = simplexml_load_string($contents);
-
-        foreach($xml->item as $item) {
-          /** @var \Kkos2\KkOs2DisplayIntegrationBundle\Events\Event */
-        $event = $serializer->deserialize($item->asXml(), 'Kkos2\KkOs2DisplayIntegrationBundle\Events\Event', 'xml');
-        }
+      if (empty($options['source'])) {
+        continue;
       }
+
+      $xml = NULL;
+      try {
+        $xml = $this->fetchKultunautXml($client, $options['source']);
+      }
+      catch (\Exception $O_o) {
+        $this->logger->error('An error occured trying to get XML: ' . $O_o->getMessage());
+        continue;
+      }
+
+      $events = [];
+      $now = new \DateTime();
+      foreach ($xml->item as $item) {
+        $startdato = \DateTime::createFromFormat('d.m.Y', $item->startdato->item);
+        $slutdato = \DateTime::createFromFormat('d.m.Y', $item->slutdato->item);
+        // If the event is old or we have hit the max number of desired events,
+        // then skip the item.
+        if ((count($events) >= $options['rss_number']) || $startdato < $now || $slutdato < $now) {
+          break;
+        }
+        $tid = (string) $item->tid->item[0];
+        $eventItem = [
+          'title' => (string) $item->overskrift,
+          'description' => (string) $item->kortbeskrivelse,
+          'date' => $startdato->format('Y-m-d'),
+          'tid' => $tid,
+          'dateandtime' => $startdato->format('l \d. j. F') . ' kl. ' . $tid,
+        ];
+        // Low-tech way to avoid duplicates.
+        $events[(string)$item->nid] = $eventItem;
+      }
+
+      $externalData = [
+        'events' => array_values($events),
+      ];
+      $slide->setExternalData($externalData);
+      // Write to the db.
+      $entityManager = $this->container->get('doctrine')->getManager();
+      $entityManager->flush();
     }
   }
+
+  /**
+   * Get a SimpleXmlElement with data from the feed url.
+   *
+   * @param Client $client
+   *   The client to (re)use.
+   * @param string $xml_url
+   *   The url to fetch the XML from.
+   *
+   * @throws \UnexpectedValueException
+   *   If the feed is malformed or does not contain events.
+   *
+   * @return \SimpleXMLElement
+   *   The xml object parsed from the feed.
+   */
+  private function fetchKultunautXml(Client $client, $xml_url) {
+    $xml = false;
+    try {
+      $response = $client->get($xml_url, [
+        'headers' => [
+          'Accept' => 'application/xml'
+        ]
+      ]);
+      $body = $response->getBody();
+      $contents = (string) $body;
+      libxml_use_internal_errors(true);
+      $xml = simplexml_load_string($contents);
+    } catch (TransferException $exception) {
+      $this->logger->error('Could not fetch Kultutnaut feed from: ' . $xml_url);
+    }
+
+    // If the parsing failed, then try to log the errors and then throw an
+    // exception.
+    if (false === $xml) {
+      $errors = [];
+      foreach (libxml_get_errors() as $error) {
+        $errors[] = $error->message;
+      }
+      $this->logger->error("Could not parse feed: \n" . implode("\n", $errors));
+      throw new \UnexpectedValueException('An error occured when trying to parse Kultunaut feed: ' . $xml_url);
+    }
+
+    // If the feed is empty, that's an error too.
+    if ($xml->item->count() < 1) {
+      throw new \UnexpectedValueException('It seems that the Kultunaut feed: ' . $xml_url . ' does not contain any events.');
+    }
+
+    return $xml;
+  }
+
 }
