e0d32eb048e02fcc9e8d818e6d073fc8a650013f
Mads H. Danquah <mads@danquah.dk>
2019-02-06 10:22:29 +0100

    Configure symfony to use the database for session-storage.
    This way the sessions will survive the situation where we clear the non-app file-
    system between deployments.

---
+++ app/DoctrineMigrations/Version20190206085935.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190206085935 extends AbstractMigration
+{
+  /**
+   * @param Schema $schema
+   */
+  public function up(Schema $schema)
+  {
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    $this->addSql('CREATE TABLE php_sessions (sess_id VARCHAR(128) NOT NULL PRIMARY KEY, sess_data BLOB NOT NULL, sess_time INTEGER UNSIGNED NOT NULL, sess_lifetime MEDIUMINT NOT NULL) COLLATE utf8_bin, ENGINE = InnoDB');
+
+  }
+
+  /**
+   * @param Schema $schema
+   */
+  public function down(Schema $schema)
+  {
+    // this down() migration is auto-generated, please modify it to your needs
+    $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    $this->addSql('DROP TABLE php_sessions');
+  }
+}
