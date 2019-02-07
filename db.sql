113a2b061ad58f2f473f1f6d8e6dafc161d9e477
Camilla Krag Jensen <ckj@reload.dk>
2019-02-07 10:59:05 -0600

    Vertical slides

---
+++ db.sql
@@ -0,0 +1,8 @@
+INSERT INTO `ik_screen` VALUES (1,'full-screen','Screen 1',1549474711,'',118287593,1,1549474722,'','{}');
+
+INSERT INTO `ik_channel` VALUES (1,'Channel 1',1549474691,1,1549474703,NULL,'ac58d04c2138c4c5c711328f8515a55875159cdc','\"[1]\"',NULL,NULL,NULL,NULL,NULL,'[]');
+
+INSERT INTO `ik_channel_screen_regions` VALUES (2,1,1,NULL,1,1);
+
+INSERT INTO `ik_channelslideorder` VALUES (1,1,1,0);
+
