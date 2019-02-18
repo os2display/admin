43868c48e5e0a92ad5ba217942060d2971cff849
Troels Ugilt Jensen <tujensen@gmail.com>
2019-02-18 15:13:04 +0100

    OS2DISP-154: Updated bundles

--- update_bundles.sh
+++ update_bundles.sh
@@ -3,4 +3,4 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 composer="$(which composer)"
-php -d memory_limit=-1 $composer update symfony/symfony os2display/core-bundle os2display/admin-bundle os2display/default-template-bundle os2display/media-bundle itk-os2display/campaign-bundle
+php -d memory_limit=-1 $composer update symfony/symfony os2display/core-bundle os2display/admin-bundle os2display/default-template-bundle os2display/media-bundle os2display/campaign-bundle
