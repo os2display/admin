da2a077e6fd6fb75d865c498f8fba2fa4a884315
Camilla Krag Jensen <ckj@reload.dk>
2019-03-18 16:56:30 -0600

    Styling for all slides with responsive

--- src/kkos2-display-bundle/Resources/public/assets/js/kk-slide-ratio.js
+++ src/kkos2-display-bundle/Resources/public/assets/js/kk-slide-ratio.js
@@ -7,9 +7,9 @@ if (!window.kkSlideRatio.getRatio) {
   window.kkSlideRatio.getRatio = function () {
     var ratio = (window.screen.height > window.screen.width) ? '9-16' : '16-9';
     if (document.querySelectorAll('.half-split').length > 0) {
-      ratio = '9-8';
-    } else if (document.querySelectorAll('.two-rows-portrait').length > 0) {
       ratio = '8-9';
+    } else if (document.querySelectorAll('.two-rows-portrait').length > 0) {
+      ratio = '9-8';
     }
     return ratio;
   };
