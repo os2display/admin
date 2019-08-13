fe006e22d973bca8d9487404e426cb1dde996d71
Mads H. Danquah <mads@danquah.dk>
2019-08-13 14:57:52 +0200

    Adding in docker-only configurations used for development

---
+++ docker/screen-config.js
@@ -0,0 +1,33 @@
+/**
+ * Sets up the config for the app.
+ */
+window.config = {
+  // Used to activate screen (rest API) and load resources.
+  "resource": {
+    "server": "//screen.kff-os2display.docker/",
+    "uri": 'proxy'
+  },
+  // Used by web-socket.
+  "ws": {
+    "server": "//screen.kff-os2display.docker/"
+  },
+  // API key to use.
+  "apikey": "059d9d9c50e0c45b529407b183b6a02f",
+  // If cookie is secure it's only send over https.
+  "cookie": {
+    "secure": false
+  },
+  // Display debug messages.
+  "debug": false,
+  // Software version.
+  "version": "test",
+  // itkLog configuration.
+  "itkLog": {
+    "version": "1",
+    "errorCallback": null,
+    "logToConsole": true,
+    "logLevel": "all"
+  },
+  // Fallback image url, (null = default), else set to "assets/images/fallback_override.png" and add the image
+  fallback_image: null
+};
